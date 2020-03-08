f5127bb6ab58d538a9d972b0da099a864ac6f11f
tyone-jp <tyonetrap@icloud.com>
2020-03-09 00:29:09 +0900

    a

--- formula.cpp
+++ formula.cpp
@@ -1,7 +1,8 @@
 //素数の判定
 bool is_prime(ll a) {
     bool ok = true;
-    if (a == 2) return ok;
+    if (a == 1) return false;
+    if (a == 2) return true;
     else {
         rep(i,sqrt(a)) {
             if (a % (i+2) == 0) ok = false;
