a5d31bda0203729190aacb060746a2f7c659a5d5
tyone-jp <tyonetrap@icloud.com>
2020-03-22 13:10:46 +0900

    a

--- graph.cpp
+++ graph.cpp
@@ -15,3 +15,39 @@ bool dfs(const Graph &g, int v, int cur = 0) {
     return true;
 }
 
+
+//union_find
+struct union_find {
+    //親を定義
+    vector<int> parent;
+
+    union_find(int n) {
+        //全ての親を−１に初期化
+        parent = vector<int>(n,-1);
+    }
+
+    //親を返す
+    int root(int a) {
+        if (parent[a] < 0) return a;
+        //子ノードを全て根に繋ぐ
+        return parent[a] = root(parent[a]);
+    }
+
+    int size(int a) {
+        //木のサイズ＝ー根の親の値
+        return -parent[root(a)];
+    }
+
+    bool connect(int a, int b) {
+        a = root(a);
+        b = root(b);
+        if (a == b) return false;
+        //大きいaに小さいbを繋ぐように調整
+        if (size(a) < size(b)) swap(a,b);
+        //サイズを更新
+        parent[a] += parent[b];
+        //木を繋ぐ
+        parent[b] = a;
+        return true;
+    }
+};
---
+++ template.cpp
@@ -0,0 +1,3 @@
+//chmin,chmax
+template<class T> bool chmin(T &a, T b) {if(a>b) {a=b;return 1;}return 0;}
+template<class T> bool chmax(T &a, T b) {if(a<b) {a=b;return 1;}return 0;}
