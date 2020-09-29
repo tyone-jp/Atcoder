7ac5e099f7b47f653a3f2b129e4895c494faa168
tyone-jp <tyonetrap@icloud.com>
2020-09-29 11:28:17 +0900

    a

--- lazy_segment_tree.cpp
+++ lazy_segment_tree.cpp
@@ -1,20 +1,20 @@
 template<class X, class M>  
 struct lazy_segment_tree {
-    using FX = function<X(X,X)>;
-    using FA = function<X(X,M)>;
-    using FM = function<M(M,M)>;
+    using Op = function<X(X,X)>;
+    using Mapping = function<X(X,M)>;
+    using Composition = function<M(M,M)>;
     int n;     
     vector<X> dat;
     vector<M> lazy;
-    FX fx;
-    FA fa;
-    FM fm;
+    Op op;
+    Mapping mapping;
+    Composition composition;
     //単位元
     X ex;
     M em;
 
-    lazy_segment_tree(int n_, FX fx_, FA fa_, FM fm_, X ex_, M em_) 
-        : fx(fx_), fa(fa_), fm(fm_), ex(ex_), em(em_) {
+    lazy_segment_tree(int n_, Op op_, Mapping mapping_, Composition composition_, X ex_, M em_) 
+        : op(op_), mapping(mapping_), composition(composition_), ex(ex_), em(em_) {
         n = 1;
         while (n < n_) n *= 2;
         dat.assign(2 * n - 1, ex);
@@ -24,17 +24,17 @@ struct lazy_segment_tree {
     void set(int k, X x) {dat[k + n - 1] = x;}
     void build() {
         for (int k = n - 2; k >= 0; k--) {
-            dat[k] = fx(dat[2 * k + 1], dat[2 * k + 2]);
+            dat[k] = op(dat[2 * k + 1], dat[2 * k + 2]);
         }
     }
 
     void eval(int k) {
         if (lazy[k] == em) return ;
         if (k < n - 1) {        // 子に伝播
-            lazy[k * 2 + 1] = fm(lazy[k * 2 + 1], lazy[k]);
-            lazy[k * 2 + 2] = fm(lazy[k * 2 + 2], lazy[k]);
+            lazy[k * 2 + 1] = composition(lazy[k * 2 + 1], lazy[k]);
+            lazy[k * 2 + 2] = composition(lazy[k * 2 + 2], lazy[k]);
         }
-        dat[k] = fa(dat[k], lazy[k]);   // 自身を更新
+        dat[k] = mapping(dat[k], lazy[k]);   // 自身を更新
         lazy[k] = em;
     }
 
@@ -43,12 +43,12 @@ struct lazy_segment_tree {
         if (r == -1) r = n;
         eval(k);
         if (a <= l && r <= b) {
-            lazy[k] = fm(lazy[k], x);
+            lazy[k] = composition(lazy[k], x);
             eval(k);
         } else if (a < r && l < b) {
             update(a, b, x, k * 2 + 1, l, (l + r) / 2);
             update(a, b, x, k * 2 + 2, (l + r) / 2, r);
-            dat[k] = fx(dat[k * 2 + 1], dat[k * 2 + 2]);
+            dat[k] = op(dat[k * 2 + 1], dat[k * 2 + 2]);
         }
     }
 
@@ -61,7 +61,7 @@ struct lazy_segment_tree {
         else {
             X vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
             X vr = query(a, b, 2 * k + 2, (l + r) / 2, r);
-            return fx(vl, vr);
+            return op(vl, vr);
         }
     }
 
--- primal_dual.cpp
+++ primal_dual.cpp
@@ -1,63 +1,119 @@
 //最小費用流
+template<class flow_t, class cost_t>
 struct primal_dual {
-    struct edge {int to, cap, cost, rev;};
+    using Pui = pair<cost_t, int>;
+    struct edge {
+        int to, rev;
+        flow_t cap;
+        cost_t cost;
+        edge(int to, flow_t cap, cost_t cost, int rev) 
+            : to(to), cap(cap), cost(cost), rev(rev) {}
+    };
     int V;
+    bool has_negative_edge = false;
+    cost_t CINF;
+    flow_t FINF;
     vector<vector<edge>> G;
-    vector<int> h, dist, prevv, preve;
+    vector<int> prevv, preve;
+    vector<cost_t> h, dis;
 
-    primal_dual(int n) {init(n);}
+    primal_dual(int n, flow_t FINF_, cost_t CINF_) 
+        : V(n), FINF(FINF_), CINF(CINF_), G(n), prevv(n, -1), preve(n, -1), h(n, 0), dis(n) {}
 
-    void init (int n) {
-        V = n;
-        G.resize(n);
-        h.resize(n);
-        dist.resize(n);
-        prevv.resize(n);
-        preve.resize(n);
+    void add_edge(int from, int to, flow_t cap, cost_t cost) {
+        has_negative_edge |= cost < 0;
+        G[from].push_back((edge){to, cap, cost, (int)G[to].size()});
+        G[to].push_back((edge){from, 0, -cost, (int)G[from].size() - 1});
     }
 
-    void add_edge(int from, int to, int cap, int cost) {
-        G[from].push_back((edge){to,cap,cost,(int)G[to].size()});
-        G[to].push_back((edge){from,0,-cost,(int)G[from].size()-1});
-    }
-
-    int min_cost_flow(int s, int t, int f) {
-        int res = 0;
-        rep(i,V) h[i] = 0;
-        while (f > 0) {
-            priority_queue<P,vector<P>,greater<P>> que;
-            rep(i,V) dist[i] = inf;
-            dist[s] = 0;
-            que.push(P(0,s));
-            while (!que.empty()) {
-                P p = que.top(); que.pop();
-                int v = p.second;
-                if (dist[v] < p.first) continue;
-                for (int i = 0; i < G[v].size(); i++) {
-                    edge &e = G[v][i];
-                    if (e.cap > 0 && dist[e.to] > dist[v] + e.cost + h[v] - h[e.to]) {
-                        dist[e.to] = dist[v] + e.cost + h[v] - h[e.to];
-                        prevv[e.to] = v;
-                        preve[e.to] = i;
-                        que.push(P(dist[e.to],e.to));
+    void bellman_ford(int s) {
+        rep(i,V) dis[i] = CINF;
+        dis[s] = 0;
+        bool is_updated = true;
+        rep(step,V) {
+            is_updated = false;
+            rep(i,V) {
+                if (dis[i] == CINF) continue;
+                rep(j,G[i].size()) {
+                    edge e = G[i][j];
+                    if (e.cap > 0 && dis[e.to] > dis[i] + e.cost) {
+                        dis[e.to] = dis[i] + e.cost;
+                        prevv[e.to] = i;
+                        preve[e.to] = j;
+                        is_updated = true;
                     }
                 }
             }
-            if (dist[t] == inf) return -1;
-            for (int v = 0; v < V; v++) h[v] += dist[v];
-            
-            int d = f;
-            for (int v = t; v != s; v = prevv[v]) {
-                d = min(d,G[prevv[v]][preve[v]].cap);
-            }
-            f -= d;
-            res += d * h[t];
-            for (int v = t; v != s; v = prevv[v]) {
-                edge &e = G[prevv[v]][preve[v]];
-                e.cap -= d;
-                G[v][e.rev].cap += d;
+            if (!is_updated) break;
+        }
+        rep(i,V) {
+            if (dis[i] != CINF) h[i] += dis[i];
+        }
+    }
+
+    void dijkstra(int s) {
+        priority_queue<Pui, vector<Pui>, greater<Pui>> que;
+        rep(i,V) dis[i] = CINF;
+        dis[s] = 0;
+        que.push(Pui(0, s));
+        while (!que.empty()) {
+            Pui p = que.top(); que.pop();
+            int v = p.second;
+            if (dis[v] < p.first) continue;
+            for (int i = 0; i < G[v].size(); i++) {
+                edge &e = G[v][i];
+                if (e.cap > 0 && dis[e.to] > dis[v] + e.cost + h[v] - h[e.to]) {
+                    dis[e.to] = dis[v] + e.cost + h[v] - h[e.to];
+                    prevv[e.to] = v;
+                    preve[e.to] = i;
+                    que.push(Pui(dis[e.to], e.to));
+                }
             }
         }
-        return res;
+        for (int v = 0; v < V; v++) {
+            if (dis[v] != CINF) h[v] += dis[v];
+        }
+    }
+
+    cost_t cal(int s, int t, flow_t f) {
+        flow_t d = f;
+        for (int v = t; v != s; v = prevv[v]) {
+            d = min(d, G[prevv[v]][preve[v]].cap);
+        }
+        f -= d;
+        for (int v = t; v != s; v = prevv[v]) {
+            edge &e = G[prevv[v]][preve[v]];
+            e.cap -= d;
+            G[v][e.rev].cap += d;
+        }
+        return h[t] * d;
+    }
+
+    cost_t run(int s, int t, flow_t f) {
+        cost_t res = 0;
+        if (has_negative_edge) {
+            bellman_ford(s);
+            if (dis[t] == CINF) return CINF;
+            res += cal(s, t, f);
+        }
+        while (f > 0) {
+            dijkstra(s);
+            if (dis[t] == CINF) return CINF;            
+            res += cal(s, t, f);
+        }
+    }
+
+    //流す量が任意の場合
+    cost_t run(int s, int t) {
+        cost_t res = 0;
+        bellman_ford(s);
+        if (h[t] >= 0 || dis[t] == CINF) return res;
+        flow_t tmp = FINF;
+        res += cal(s, t, tmp);
+        while (true) {
+            dijkstra(s);
+            if (h[t] >= 0 || dis[t] == CINF) return res;
+            res += cal(s, t, tmp);
+        }
     }
 };
